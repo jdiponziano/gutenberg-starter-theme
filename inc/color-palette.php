33adc4f17ec01eef1237d3011c59a49a165bd0cf
Jessica Di Ponziano <jdiponziano@gmail.com>
2019-01-27 16:20:05 -0500

    Add custom color palette include

---
+++ inc/color-palette.php
@@ -0,0 +1,24 @@
+<?php
+
+$color_palette = [
+  [
+    'name'  => esc_html__( 'Strong Blue', 'themename' ),
+    'slug'  => 'strong-blue',
+    'color' => '#0073aa',
+	],
+	[
+    'name'  => esc_html__( 'Lighter Blue', 'themename' ),
+    'slug'  => 'lighter-blue',
+    'color' => '#229fd8',
+  ],
+	[
+    'name'  => esc_html__( 'Very Light Gray', 'themename' ),
+    'slug'  => 'very-light-gray',
+    'color' => '#eee',
+  ],
+	[
+    'name'  => esc_html__( 'Very Dark Gray', 'themename' ),
+    'slug'  => 'very-dark-gray',
+    'color' => '#444',
+	]
+];
