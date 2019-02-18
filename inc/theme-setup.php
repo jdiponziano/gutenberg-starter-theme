c54d85d708fda29412c651093e9170ce22011891
Jessica DiPonziano <jess@inverseparadox.net>
2019-02-18 12:17:50 -0500

    Add beginning of sidebar template

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -108,3 +108,21 @@ function themename_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'themename_content_width', 980 );
 }
 add_action( 'after_setup_theme', 'themename_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function themename_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Default Sidebar', 'themename' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'themename' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'themename_widgets_init' );
